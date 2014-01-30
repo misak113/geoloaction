df3a75c1b55db382a1869e649f1228c5f2b925fe
Michael Zabka <zabka.michael@seznam.cz>
2014-01-31 00:44:04 +0100

    write device position no algoritm

--- app_modules/triangulation/public/build/app.js
+++ app_modules/triangulation/public/build/app.js
@@ -3,14 +3,18 @@
 function TriangulationCtrl($scope, $http, $timeout) {
 	var loungeCanvas = document.getElementById("lounge").getContext('2d');
 	var apSize = 10;
+	var deviceSize = 7;
 	$scope.lounge = {};
 	$scope.wifiLogs = [];
+	var width = 640;//$scope.lounge.x_length * 100;
+	var height = 480;//$scope.lounge.y_length * 100;
 
 	$scope.loadData = function () {
 		$http.get('load-lounges').success(function (data) {
 			// only first
 			$scope.lounge = data.lounges.pop();
 		});
+		refreshWifiLog();
 	};
 
 	$scope.$watch('lounge', function (lounge, loungeBefore) {
@@ -21,23 +25,46 @@ function TriangulationCtrl($scope, $http, $timeout) {
 		$http.get('load-wifi-logs').success(function (data) {
 			$scope.wifiLogs = data.logs;
 		});
+		writeWifiLogs();
 		$timeout(refreshWifiLog, 1000);
 	};
-	refreshWifiLog();
 
 	var writeApList = function (apList) {
-		var width = $scope.lounge.x_length * 100;
-		var height = $scope.lounge.y_length * 100;
 		loungeCanvas.lineWidth = 2;
 		loungeCanvas.fillStyle = '#000'; // blue
 		loungeCanvas.strokeStyle = '#555'; // blue
 		angular.forEach(apList, function (ap) {
-			console.log(width, height, ap);
 			var x = width*ap.x_position-apSize/2;
 			var y = height*ap.y_position-apSize/2;
-			console.log(x, y);
 			loungeCanvas.fillRect(x, y, apSize, apSize);
 			loungeCanvas.strokeRect(x, y, apSize, apSize);
 		});
+	};
+
+	var writeWifiLogs = function () {
+		angular.forEach($scope.wifiLogs, function (wifiLog) {
+			var devicePosition = calculatePosition(wifiLog.apList);
+			writeDevicePosition(devicePosition);
+		});
+	};
+
+	var writeDevicePosition = function (position) {
+		loungeCanvas.lineWidth = 2;
+		loungeCanvas.fillStyle = '#157'; // blue
+		loungeCanvas.strokeStyle = '#37A'; // blue
+
+		var x = width*position.x_position-deviceSize/2;
+		var y = height*position.y_position-deviceSize/2;
+		loungeCanvas.fillRect(x, y, deviceSize, deviceSize);
+		loungeCanvas.strokeRect(x, y, deviceSize, deviceSize);
+
+	};
+
+	var calculatePosition = function (logApList) {
+		// algoritmus triangulace
+		return {
+			x_position: 0.4,
+			y_position: 0.5
+		};
 	};
 }
