92f7fd084412823585362909ba29937337ea5ba1
Michael Zabka <zabka.michael@seznam.cz>
2014-02-02 20:46:42 +0100

    added package npm deps

--- mysql_tables/tables_schema.sql
+++ mysql_tables/tables_schema.sql
@@ -211,3 +211,11 @@ CREATE TABLE `wifi_log_ap` (
 
 
 -- 2014-02-02 16:11:11
+
+ALTER TABLE `ap_log`
+CHANGE `ap_code` `ap_code` varchar(50) COLLATE 'utf8_general_ci' NOT NULL COMMENT 'kód ap wifi routeru' AFTER `ap_log_id`,
+CHANGE `date_access` `date_access` datetime NOT NULL COMMENT 'datum pořízení logu' AFTER `ap_code`,
+CHANGE `role` `role` varchar(50) COLLATE 'utf8_general_ci' NOT NULL COMMENT 'Role ve které zařízení vystupuje. Zatím pouze Client' AFTER `date_access`,
+CHANGE `mac_hash` `mac_hash` varchar(50) COLLATE 'utf8_general_ci' NOT NULL COMMENT 'zahashovaná mac adresa zařízení' AFTER `role`,
+CHANGE `type` `type` varchar(50) COLLATE 'utf8_general_ci' NOT NULL COMMENT 'typ záznamu. připojen, odpojen z ap' AFTER `mac_hash`,
+COMMENT=''; -- 15.572 s
